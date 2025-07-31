f73dc50010bbf58e6656b3de209c4c9515ba269b
admirec <97503470+octaviusthe3rd@users.noreply.github.com>
2025-07-31 15:49:06 +0200

    twilio handler

---
+++ api/utils/twilio.js
@@ -0,0 +1,22 @@
+const twilio = require('twilio');
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+
+const client = twilio(accountSid, authToken);
+
+const sendSms = async (to, body) => {
+  try {
+    await client.messages.create({
+      body,
+      from: twilioPhoneNumber,
+      to,
+    });
+    console.log(`SMS sent to ${to}`);
+  } catch (error) {
+    console.error(`Error sending SMS: ${error.message}`);
+  }
+};
+
+module.exports = { sendSms };
